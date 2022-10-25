7da341389050464c4eb43e914157f04fda3e76bc
Matheus <matheus.gregorin@sptech.school>
2022-10-25 10:40:06 -0300

    outro teste

--- aplicacao-specula/src/main/java/com/mycompany/tela/login/Conexao.java
+++ aplicacao-specula/src/main/java/com/mycompany/tela/login/Conexao.java
@@ -18,7 +18,7 @@ public class Conexao {
 
         dataSource​.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-        dataSource​.setUrl("jdbc:mysql://localhost:3306/specula"); // MUDAR DE "MYSQL" PARA ENDEREÇO IP DO HOSTNAME DA MAQUINA VIRTUAL
+        dataSource​.setUrl("jdbc:mysql://172.31.16.213:3306/specula"); // MUDAR DE "MYSQL" PARA ENDEREÇO IP DO HOSTNAME DA MAQUINA VIRTUAL
 
         dataSource​.setUsername("root");
 
